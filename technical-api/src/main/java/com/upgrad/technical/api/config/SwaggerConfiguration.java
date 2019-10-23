b9e0a4b3fa2e896f1a213cb64f4dae723a44c1de
avia-fernandes <avia.fernandes@gmail.com>
2019-10-23 20:15:04 +0300

    Test commit: added comment in SwaggerConfig

--- technical-api/src/main/java/com/upgrad/technical/api/config/SwaggerConfiguration.java
+++ technical-api/src/main/java/com/upgrad/technical/api/config/SwaggerConfiguration.java
@@ -9,7 +9,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+//Test Comments by Avia
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
